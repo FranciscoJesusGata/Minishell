513cd3d18d2b7d2c90125d37e4ad9003d1943406
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2021-10-15 13:43:22 +0200

    readline test, prompt broken

--- src/term_utils_2.c
+++ src/term_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 12:39:35 by fgata-va          #+#    #+#             */
-/*   Updated: 2021/05/25 18:50:35 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/10/15 13:09:01 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ void	write_buffer(char **buffer, char input)
 	*(*buffer) = 0;
 }
 
-void	delete_buffer(char **buffer)
+/*void	delete_buffer(char **buffer)
 {
 	*buffer -= 1;
 	**buffer = 0;
 	tputs(tgetstr("le", NULL), 1, ft_putchar);
 	tputs(tgetstr("dc", NULL), 1, ft_putchar);
-}
+}*/
