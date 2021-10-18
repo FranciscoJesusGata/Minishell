d340a96b77309bfbbb0e4d37d4310d485438ccee
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2021-10-18 17:06:29 +0200

    Relink fixed and extra line break deleted

--- src/term_utils.c
+++ src/term_utils.c
@@ -6,30 +6,11 @@
 /*   By: fgata-va <fgata-va@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 16:32:03 by fgata-va          #+#    #+#             */
-/*   Updated: 2021/10/15 13:08:43 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/10/18 16:59:49 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	prompt(int exit_code)
-{
-	ft_putstr_fd("\e[38;5;38mMiniShell ~ \e[0m", 1);
-	if (!exit_code)
-		ft_putstr_fd("👉 ", 1);
-	else
-		ft_putstr_fd("👎 ", 1);
-}
-
-/*void	sound_bell(void)
-{
-	tputs(tgetstr("bl", NULL), 1, ft_putchar);
-}*/
-
-/*void	clean_screen(void)
-{
-	tputs(tgetstr("cl", NULL), 1, ft_putchar);
-}*/
 
 void	welcome(void)
 {
@@ -60,10 +41,3 @@ void	welcome(void)
 	ft_putstr_fd("@fgata-va\e[0m\n", 1);
 }
 
-/*void	clear_line(t_term *term)
-{
-	tputs(tgoto(tgetstr("ch", NULL), 0, 15), 1, ft_putchar);
-	tputs(tgetstr("ce", NULL), 1, ft_putchar);
-	term->cursor = term->buffer;
-	*term->cursor = 0;
-}*/
