14817ed5df04c9b49ae57e5190afd7d8eac418ad
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2021-10-18 17:32:56 +0200

    Deleted unused files, deleted unused functions, main readapted to project needs. Maybe this is a too crazy commit

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fportalo <fportalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 09:18:14 by fportalo          #+#    #+#             */
-/*   Updated: 2021/10/15 12:46:59 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/10/18 18:00:59 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,9 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
-# include <term.h>
-# include "structs.h"
 
 int		main();
-int		launch_term(void);
-void	minishell(void);
+char	*launch_term(void);
 
 /*
 ** Utils
@@ -38,25 +35,6 @@ int		ft_strcmp(const char *s1, const char *s2);
 ** Term utils
 */
 
-void	prompt(int exit_code);
-void	sound_bell(void);
 void	welcome(void);
-void	clear_line(t_term *term);
-void	clean_screen(void);
-
-/*
-** History handling
-*/
-
-void	free_history(t_hist *history);
-void	restore_position(t_hist **history);
-t_hist	*ft_dlstnew(char const *command);
-void	ft_dlst_prepend(t_hist *current, t_hist *new_element);
-void	restore_original(t_hist *history);
-void	write_buffer(char **buffer, char input);
-void	delete_buffer(char **buffer);
-void	save_command(t_term *term, char *command);
-void	navigate_history(t_term *term, int dir);
-void	print_history(t_hist *history);
 
 #endif
--- src/terminal.c
+++ src/terminal.c
@@ -6,117 +6,18 @@
 /*   By: fgata-va <fgata-va@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 14:51:58 by fgata-va          #+#    #+#             */
-/*   Updated: 2021/10/18 17:01:41 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/10/18 17:17:54 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
-void	init_term(t_term *term)
-{
-	tgetent (term->buffer, getenv("TERM"));
-	term->cursor = term->buffer;
-	ft_bzero(&term->config, sizeof(struct termios));
-	tcgetattr(STDIN_FILENO, &term->backup);
-	term->config = term->backup;
-	term->config.c_lflag &= ~(ICANON | ECHO);
-	term->config.c_cc[VMIN] = 1;
-	term->config.c_cc[VTIME] = 0;
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &term->config);
-	tputs(tgetstr("ks", NULL), 1, ft_putchar);
-	*term->buffer = 0;
-	term->history = ft_dlstnew("");
-	term->h_cursor = term->history;
-	term->cursor = term->buffer;
-}
-
-void	test_commands(t_term *term)
-{
-	if (!(ft_strncmp(term->buffer, "welcome", 8)))
-		welcome();
-	if (!(ft_strncmp(term->buffer, "exit", 5)))
-	{
-		tcsetattr(STDIN_FILENO, TCSAFLUSH, &term->backup);
-		free_history(term->history);
-		exit(0);
-	}
-	if (!(ft_strncmp(term->buffer, "history", 7)))
-		print_history(term->history);
-	if (!(ft_strncmp(term->buffer, "clear", 5)))
-		clean_screen();
-}
-
-int	enter_command(t_term *term)
-{
-	write(1, "\r\n", 2);
-	if (*term->buffer)
-	{
-		save_command(term, term->buffer);
-		printf("\"%s\"\n", term->buffer);
-		test_commands(term); //Aquí integras la función de Lexer
-		term->cursor = term->buffer;
-		*term->cursor = 0;
-	}
-	return (0);
-}*/
-
-/*int	launch_term(void)
-{
-	t_term	term;
-	char	buff[9];
-	int		readed;
-	int		i;
-
-	welcome();
-	init_term(&term);
-	prompt(0);
-	readed = read(STDIN_FILENO, &buff, 8);
-	while (readed > 0)
-	{
-		i = 0;
-		while (i < readed)
-		{
-			if (buff[i] == '\n')
-				prompt(enter_command(&term));
-			else if (buff[i] == '\033')
-			{
-				if (!(ft_strncmp(buff + i, "\033OA", 3)))
-					navigate_history(&term, 1);
-				else if (!(ft_strncmp(buff + i, "\033OB", 3)))
-					navigate_history(&term, 0);
-				else
-					sound_bell();
-				break ;
-			}
-			else if (buff[i] == 127 && term.cursor != term.buffer)
-				delete_buffer(&term.cursor);
-			else if (buff[i] >= ' ' && buff[i] < 127)
-				write_buffer(&term.cursor, buff[i]);
-			else
-				sound_bell();
-			i++;
-		}
-		readed = read(STDIN_FILENO, &buff, 8);
-	}
-	free_history(term.history);
-	return (0);
-}*/
-
-int		launch_term(void)
+char		*launch_term(void)
 {
 	char	*line;
 
-	welcome();
-	while (1)
-	{
-		line = readline("\001\e[38;5;38m\002MiniShell ~ 👉 \001\e[0m\002");
-		if (line && *line)
-			add_history(line);
-		else if (!line)
-			break;
-		free(line);
-	}
-	clear_history();
-	return (0);
+	line = readline("\001\e[38;5;38m\002MiniShell ~ 👉 \001\e[0m\002");
+	if (line && *line)
+		add_history(line);
+	return (line);
 }
