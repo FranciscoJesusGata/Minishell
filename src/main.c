ba55e0c070eb2360d23c95124201b7a7e012a015
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2021-10-18 17:32:56 +0200

    Deleted unused files, deleted unused functions, main readapted to project needs. Maybe this is a too crazy commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fportalo <fportalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 09:18:25 by fportalo          #+#    #+#             */
-/*   Updated: 2021/10/15 13:12:18 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/10/18 17:25:54 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,17 @@
 
 int		main()
 {
-	return(launch_term());
+	char *line;
+
+	welcome();
+	while (1)
+	{
+		line = launch_term();
+		if (!line)
+			break;
+		if (*line)
+			printf("\"%s\"\n", line);
+		free(line);
+	}
+	clear_history();
 }
