fe6c1642e54e32e232a1d66fa0155ba645136aff
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2021-11-09 18:08:08 +0100

    paste bug in prompt solved

--- src/terminal.c
+++ src/terminal.c
@@ -6,7 +6,7 @@
 /*   By: fportalo <fportalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/19 14:51:58 by fgata-va          #+#    #+#             */
-/*   Updated: 2021/10/29 12:01:56 by fportalo         ###   ########.fr       */
+/*   Updated: 2021/11/09 18:07:37 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*launch_term(void)
 {
 	char	*line;
 
-	line = readline("\001\e[38;5;38m\002MiniShell ~ 👉 \001\e[0m\002");
+	line = readline("\001\e[38;5;38m\002MiniShell ~ 👉  \001\e[0m\002");
 	if (line && *line)
 		add_history(line);
 	return (line);
