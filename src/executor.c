1fabc207893e59be1ee1796b7fb2e3d9ab81fe5f
pakoportalo <pakoportalo@gmail.com>
2021-11-26 12:47:01 +0100

    proto executor working

--- src/executor.c
+++ src/executor.c
@@ -6,16 +6,21 @@
 /*   By: fportalo <fportalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 15:08:07 by fportalo          #+#    #+#             */
-/*   Updated: 2021/11/25 17:58:49 by fportalo         ###   ########.fr       */
+/*   Updated: 2021/11/26 12:46:08 by fportalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	is_error(t_cmd *cmd)
+{
+	printf("minishell: %s: command not found\n", cmd->cmds->argv[0]);
+}
 
-void	is_builtin(t_cmd *cmd, t_env *env)
+int	is_builtin(t_cmd *cmd, t_env *env)
 {
 	env = NULL;
+	printf("\n");
 	if (!ft_strncmp(cmd->cmds->argv[0], "pwd", 3))
 		printf("executing pwd...\n");
 	else if (!ft_strncmp(cmd->cmds->argv[0], "env", 3))
@@ -30,9 +35,59 @@ void	is_builtin(t_cmd *cmd, t_env *env)
 		printf("executing cd...\n");
 	else if(!ft_strncmp(cmd->cmds->argv[0], "exit", 4))
 		printf("executing exit...\n");
+	else
+		return (0);
+	return (1);
+}
+
+char	*find_binary(t_cmd *cmd, t_env *env)
+{
+	int i;
+	int fd;
+	char *path;
+
+	i = 0;
+	while (env->path[i])
+	{
+		path = ft_strdup(env->path[i]);
+		path = ft_strjoin(path, "/");
+		path = ft_strjoin(path, cmd->cmds->argv[0]);
+		fd = open(path, O_RDONLY);
+		if (fd > 0)
+			return (path);
+		else
+			free(path);
+		close(fd);
+		i++;
+	}
+	return (NULL);
+}
+
+int	is_binary(t_cmd *cmd, t_env *env, int i)
+{
+	char *path;
+
+	if (!i)
+	{
+		path = find_binary(cmd, env);
+		if (path)
+		{
+			printf("\nPath is: %s\n",path);
+			return (1);
+		}
+	}
+	return (0);
 }
 
 void	executor(t_env *env, t_cmd *cmd)
 {
-	is_builtin(cmd, env);
+	int i;
+
+	i = 0;
+	if (is_builtin(cmd, env))
+		i = 1;
+	else if(is_binary(cmd, env, i))
+		i = 1;
+	if (i == 0)
+		is_error(cmd);
 }
