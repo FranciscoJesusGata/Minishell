85408a8ecae1710932b6d7a8d0d042f8e8ca15ce
pakoportalo <pakoportalo@gmail.com>
2021-11-30 16:57:49 +0100

    export fix envs with/without =

--- src/ft_export.c
+++ src/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: fportalo <fportalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 15:28:42 by fportalo          #+#    #+#             */
-/*   Updated: 2021/11/29 17:57:32 by fportalo         ###   ########.fr       */
+/*   Updated: 2021/11/30 16:57:09 by fportalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,20 @@ char	*include_quotes(t_env *env, char *ret, int i)
 	char **split;
 
 	split = ft_split(env->all[i], '=');
-	ret = ft_strjoin(split[0], "=");
-	ret = ft_strjoin(ret, "\"");
-	ret = ft_strjoin(ret, split[1]);
-	ret = ft_strjoin(ret, "\"");
+	if (split[1])
+	{
+		ret = ft_strjoin(split[0], "=");
+		ret = ft_strjoin(ret, "\"");
+		ret = ft_strjoin(ret, split[1]);
+		ret = ft_strjoin(ret, "\"");
+	}
+	else if((ft_strchr(env->all[i], '=')))
+		ret = ft_strjoin(env->all[i], "\"\"");
+	else
+		ret = ft_strdup(env->all[i]);
 	return (ret);
 }
+
 
 char **env_to_temp(t_env *env, char **temp)
 {
@@ -64,11 +72,11 @@ char	**bubble_sort(char **array, int array_size)
 		j = 0;
 		while (j < array_size - i - 1)
 		{
-			if(ft_strncmp(array[j], array[j + 1], 4) > 0)
+			if (ft_strncmp(array[j], array[j + 1], 4) > 0)
 			{
 				tmp = array[j];
 				array[j] = array[j+1];
-				array[j+1] = tmp;
+				array[j + 1] = tmp;
 			}
 			j++;
 		}
