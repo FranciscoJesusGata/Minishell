8e91706eebacbc713fb200d2f5e33ec3a0b7472a
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2021-12-13 16:09:30 +0100

    Executor scheme done

--- src/executor.c
+++ src/executor.c
@@ -6,39 +6,44 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 15:08:07 by fportalo          #+#    #+#             */
-/*   Updated: 2021/12/11 19:57:07 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/12/13 16:08:29 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	is_error(t_simpleCmd *cmd, char **env_path)
+int is_builtin(char *cmd)
 {
-	if (!ft_strchr(cmd->argv[0], '/') || !env_path)
-		printf("minishell: %s: No such file or directory\n", cmd->argv[0]);
-	else
-		printf("minishell: %s: command not found\n", cmd->argv[0]);
+	int	len;
+
+	len = ft_strlen(cmd);
+	if (!ft_strncmp(cmd, "pwd", len) || !ft_strncmp(cmd, "env", len) ||
+		!ft_strncmp(cmd, "echo", len) || !ft_strncmp(cmd, "export", len) ||
+		!ft_strncmp(cmd, "unset", len) || !ft_strncmp(cmd, "cd", len) ||
+		!ft_strncmp(cmd, "exit", len))
+		return (1);
+	return (0);
 }
 
-int	is_builtin(t_simpleCmd *cmd, char ***env)
+void	exec_builtin(t_simpleCmd *cmd, char ***env)
 {
-	if (!ft_strncmp(cmd->argv[0], "pwd", ft_strlen("pwd")))
+	int	len;
+
+	len = ft_strlen(cmd->argv[0]);
+	if (!ft_strncmp(cmd->argv[0], "pwd", len))
 		ft_pwd();
-	else if (!ft_strncmp(cmd->argv[0], "env", ft_strlen("env")))
+	else if (!ft_strncmp(cmd->argv[0], "env", len))
 		ft_env(*env);
-	else if (!ft_strncmp(cmd->argv[0], "echo", ft_strlen("echo")))
+	else if (!ft_strncmp(cmd->argv[0], "echo", len))
 		ft_echo(cmd->argc, cmd->argv);
-	else if (!ft_strncmp(cmd->argv[0], "export", ft_strlen("export")))
+	else if (!ft_strncmp(cmd->argv[0], "export", len))
 		ft_export(cmd->argc, cmd->argv, env);
-	else if (!ft_strncmp(cmd->argv[0], "unset", ft_strlen("unset")))
+	else if (!ft_strncmp(cmd->argv[0], "unset", len))
 		ft_unset(cmd->argc, cmd->argv, env);
-	else if (!ft_strncmp(cmd->argv[0], "cd", ft_strlen("cd")))
+	else if (!ft_strncmp(cmd->argv[0], "cd", len))
 		ft_cd(cmd->argc, cmd->argv, env);
-	else if (!ft_strncmp(cmd->argv[0], "exit", ft_strlen("exit")))
+	else if (!ft_strncmp(cmd->argv[0], "exit", len))
 		ft_exit(cmd->argv[1]);
-	else
-		return (0);
-	return (1);
 }
 
 char	*find_binary(t_simpleCmd *cmd, char **path)
@@ -48,88 +53,59 @@ char	*find_binary(t_simpleCmd *cmd, char **path)
 	char	*bin_path;
 
 	i = 0;
-	while (path[i])
+	found = -1;
+	bin_path = NULL;
+	if (!path || ft_strchr(cmd->argv[0], '/'))
 	{
-		bin_path = ft_strdup(path[i]);
-		bin_path = clean_strjoin(bin_path, "/");
-		bin_path = clean_strjoin(bin_path, cmd->argv[0]);
-		found = access(bin_path, X_OK);
-		if (found == 0)
-			return (bin_path);
-		free(bin_path);
-		i++;
+		found = access(cmd->argv[0], X_OK);
+		if (found < 0)
+			printf("minishell: %s: No such file or directory\n", cmd->argv[0]);
+		else
+			bin_path = cmd->argv[0];
 	}
-	return (NULL);
+	else
+	{
+		while (path[i] && found < 0)
+		{
+			bin_path = ft_strdup(path[i]);
+			bin_path = clean_strjoin(bin_path, "/");
+			bin_path = clean_strjoin(bin_path, cmd->argv[0]);
+			found = access(bin_path, X_OK);
+			free(bin_path);
+			bin_path = NULL;
+			i++;
+		}
+	}
+	return (bin_path);
 }
 
 int	is_binary(t_simpleCmd *cmd, char **path, char **bin_path)
 {
 	*bin_path = find_binary(cmd, path);
-	if (*path)
+	if (*bin_path)
 	{
 		printf("Path is: %s\n", *bin_path);
-		free(*bin_path);
-		*bin_path = NULL;
 		return (1);
 	}
+	printf("minishell: %s: command not found\n", cmd->argv[0]);
 	return (0);
 }
 
-int	single_cmd(char ***env, char **path, t_simpleCmd *cmd)
-{
-	char		*bin_path;
-	int			pid;
-	int			exit_code;
-
-	if (is_binary(cmd, path, &bin_path))
-	{
-		pid = fork();
-		if (!pid)
-		{
-			//redirect
-			//exec
-			exit (0);
-		}
-		exit_code = waitpid(pid, NULL, 0);
-		//exit_code = get_exit_code(exit_code);
-	}
-	else
-	{
-		//redirect
-		if (!is_builtin(cmd, env))
-			is_error(cmd, path);
-		exit_code = 127;
-		//recover stdin and stdout
-	}
-	return (exit_code);
-}
-
-void	multiple_cmds(char ***env, char **path, t_simpleCmd *cmd)
+int		exec_cmd(t_simpleCmd *cmd, int is_builtin, char *env[][], char **path)
 {
 	char	*bin_path;
-	int		pid;
+	int		exit_status;
 
-	while (cmd)
-	{
-		pid = fork();
-		if (!pid)
-		{
-			//redirections
-			if (!is_builtin(cmd, env))
-			{
-				if (!is_binary(cmd, path, &bin_path))
-				{
-					is_error(cmd, path);
-					exit(127);
-				}
-				//execute
-			}
-			exit(0);
-		}
-		cmd->pid = pid;
-		cmd = cmd->nxt;
-	}
-	
+	bin_path = NULL;
+	exit_status = 0;
+	//redirections
+	if (is_builtin)
+		exec_builtin(cmd, env);
+	else if (is_binary(cmd, path, &bin_path))
+		execve(bin_path, cmd->argv, *env);
+	else
+		exit_status = 127;
+	return (exit_status);
 }
 
 char	**get_path(char **envp)
@@ -142,7 +118,11 @@ char	**get_path(char **envp)
 	while (envp[i] && ft_strncmp(envp[i], "PATH", 4))
 		i++;
 	if (envp[i])
+	{
 		paths = ft_split(envp[i] + 5, ':');
+		if (!paths)
+			malloc_error();
+	}
 	return (paths);
 }
 
@@ -150,16 +130,30 @@ int	executor(char ***env, t_cmd *cmd)
 {
 	t_simpleCmd	*s_cmd;
 	int			exit_code;
-	//char		**path;
+	char		**path;
+	int			builtin;
+	pid_t		pid;
 	
 	s_cmd = cmd->cmds;
-	exit_code = is_builtin(cmd->cmds, env);
-	/*path = get_path(*env);
-	if (cmd->count == 1)
-		exit_code = single_cmd(env, path, cmd->cmds);
+	path = get_path(*env);
+	exit_code = 0;
+	builtin = is_builtin(s_cmd);
+	if (cmd->count == 1 && builtin)
+		exec_builtin(s_cmd, env);
 	else
 	{
-		multiple_cmds(env, path, cmd->cmds);
+		s_cmd = cmd->cmds;
+		while (s_cmd)
+		{
+			pid = fork();
+			if (!pid)
+			{
+				exit_code = exec_cmd(cmd, builtin, env, path);
+				exit (exit_code);
+			}
+			s_cmd->pid = pid;
+			s_cmd = s_cmd->nxt;
+		}
 		s_cmd = cmd->cmds;
 		while (s_cmd)
 		{
@@ -168,6 +162,6 @@ int	executor(char ***env, t_cmd *cmd)
 		}
 		//exit_code = get_exit_code(exit_code);
 	}
-	ft_freearray(path);*/
+	ft_freearray(path);
 	return (exit_code);
 }
