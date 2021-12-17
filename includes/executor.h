8a0a2a624eb47a7b3b01336021bcaa262c263829
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2021-12-17 11:51:02 +0100

    Executor.h protection fixed

--- includes/executor.h
+++ includes/executor.h
@@ -6,12 +6,12 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 15:16:22 by fportalo          #+#    #+#             */
-/*   Updated: 2021/12/15 21:37:48 by fgata-va         ###   ########.fr       */
+/*   Updated: 2021/12/17 11:50:34 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXECUTOR
-# define EXECUTOR
+#ifndef EXECUTOR_H
+# define EXECUTOR_H
 # include "minishell.h"
 
 int		executor(char ***env, t_cmd *cmd);
